cb8241f3f31e26a2a7af1b379feee7988c494b8f
Shawn Crosby <scrosby@salesforce.com>
2013-08-08 17:54:22 -0300

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sc_pylibs',
-    version='0.1.0',
+    version='0.1.1',
     author='Shawn Crosby',
     author_email='sacrosby@gmail.com',
     packages=find_packages(),
